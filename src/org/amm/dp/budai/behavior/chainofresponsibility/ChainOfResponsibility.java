65df1026a61d08252718238a21e5a55091f1cffb
amm <andriy.amm@gmail.com>
2013-01-24 17:27:42 +0200

    fix chain of resposibility

--- src/org/amm/dp/budai/behavior/chainofresponsibility/ChainOfResponsibility.java
+++ src/org/amm/dp/budai/behavior/chainofresponsibility/ChainOfResponsibility.java
@@ -1,25 +1,34 @@
 package org.amm.dp.budai.behavior.chainofresponsibility;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class ChainOfResponsibility
-{
-    public static void main(String[] args)
-    {
-          Food cappuccino1 = new Food("Cappuccino", new ArrayList<String>{ "Coffee", "Milk", "Sugar" });
-        Food cappuccino2 = new Food("Cappuccino", new ArrayList<String> { "Coffee", "Milk" });
-        Food soup1 = new Food("Soup with meat", new ArrayList<String> { "Meat", "Water", "Potato" });
-        Food soup2 = new Food("Soup with potato", new ArrayList<String> { "Water", "Potato" });
-        Food meat = new Food("Meat", new ArrayList<String> { "Meat" });
+public class ChainOfResponsibility {
+	public static void main(String[] args) {
 
-        GirlFriend girlFriend = new GirlFriend(null);
-        Me me = new Me(girlFriend);
-        BestFriend bestFriend = new BestFriend(me);
+		Food cappuccino1 = new Food("Cappuccino", new ArrayList<String>(
+				Arrays.asList("Coffee", "Milk", "Sugar")));
 
-        bestFriend.HandleFood(cappuccino1);
-        bestFriend.HandleFood(cappuccino2);
-        bestFriend.HandleFood(soup1);
-        bestFriend.HandleFood(soup2);
-        bestFriend.HandleFood(meat);
-    }
+		Food cappuccino2 = new Food("Cappuccino", new ArrayList<String>(
+				Arrays.asList("Coffee", "Milk")));
+
+		Food soup1 = new Food("Soup with meat", new ArrayList<String>(
+				Arrays.asList("Meat", "Water", "Potato")));
+
+		Food soup2 = new Food("Soup with potato", new ArrayList<String>(
+				Arrays.asList("Water", "Potato")));
+
+		Food meat = new Food("Meat", new ArrayList<String>(
+				Arrays.asList("Meat")));
+
+		GirlFriend girlFriend = new GirlFriend(null);
+		Me me = new Me(girlFriend);
+		BestFriend bestFriend = new BestFriend(me);
+
+		bestFriend.HandleFood(cappuccino1);
+		bestFriend.HandleFood(cappuccino2);
+		bestFriend.HandleFood(soup1);
+		bestFriend.HandleFood(soup2);
+		bestFriend.HandleFood(meat);
+	}
 }
